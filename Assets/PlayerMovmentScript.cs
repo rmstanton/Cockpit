278aeb71a32964be6fa0720095ea58df80c3d646
joobeck <joobeck@Hotmail.com>
2014-11-06 15:56:44 -0700

    Added in initial interpretation of axes and moved movement to a new scene

--- Assets/PlayerMovmentScript.cs
+++ Assets/PlayerMovmentScript.cs
@@ -1,36 +1,80 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovmentScript : MonoBehaviour {
+public class PlayerMovmentScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public bool dontPassZero = true;
 
-	void FixedUpdate()
-	{
-		Vector3 newVel = this.rigidbody.velocity;
-		int xvel = 0, yvel = 0, zvel = 0;
-		if(this.rigidbody.velocity.x > 0)
-			xvel = 1;
-		if(this.rigidbody.velocity.x < 0)
-			xvel = -1;
-		
-		if(this.rigidbody.velocity.y > 0)
-			yvel = 1;
-		if(this.rigidbody.velocity.y < 0)
-			yvel = -1;
-		
-		if(this.rigidbody.velocity.z > 0)
-			zvel = 1;
-		if(this.rigidbody.velocity.z < 0)
-			zvel = -1;
+    // Use this for initialization
+    void Start()
+    {
+    
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
 
-	}
+    void FixedUpdate()
+    {
+        AdjustVelocity();
+        AdjustRotation();
+    }
+
+    private void AdjustVelocity()
+    {
+        
+        Vector3 newVel = this.rigidbody.velocity;
+
+        Debug.Log("Thrust is " + Input.GetAxis("Thrust"));
+        Debug.Log("YMovement is " + Input.GetAxis("YMovement"));
+
+        newVel.x += Input.GetAxis("Thrust");
+        newVel.y += Input.GetAxis("YMovement");
+        
+        if(dontPassZero)
+        {
+            if((this.rigidbody.velocity.x < 0 && newVel.x > 0) || (this.rigidbody.velocity.x > 0 && newVel.x < 0))
+                newVel.x = 0;
+            if((this.rigidbody.velocity.y < 0 && newVel.y > 0) || (this.rigidbody.velocity.y > 0 && newVel.y < 0))
+                newVel.y = 0;
+            if((this.rigidbody.velocity.z < 0 && newVel.z > 0) || (this.rigidbody.velocity.z > 0 && newVel.z < 0))
+                newVel.z = 0;
+        }
+
+        
+        this.rigidbody.velocity = newVel;
+    }
+
+    private void AdjustRotation()
+    {
+        
+        Vector3 newVel = this.rigidbody.angularVelocity;
+        
+        Debug.Log("Roll is " + Input.GetAxis("Roll"));
+        Debug.Log("Yaw is " + Input.GetAxis("Yaw"));
+        Debug.Log("Pitch is " + Input.GetAxis("Pitch"));
+        Debug.Log("");
+        
+        newVel.x += Input.GetAxis("Roll");
+        newVel.y += Input.GetAxis("Yaw");
+        newVel.z += Input.GetAxis("Pitch");
+        
+        if(dontPassZero)
+        {
+            if((this.rigidbody.angularVelocity.x < 0 && newVel.x > 0) || (this.rigidbody.angularVelocity.x > 0 && newVel.x < 0))
+                newVel.x = 0;
+            if((this.rigidbody.angularVelocity.y < 0 && newVel.y > 0) || (this.rigidbody.angularVelocity.y > 0 && newVel.y < 0))
+                newVel.y = 0;
+            if((this.rigidbody.angularVelocity.z < 0 && newVel.z > 0) || (this.rigidbody.angularVelocity.z > 0 && newVel.z < 0))
+                newVel.z = 0;
+        }
+        
+        
+        this.rigidbody.angularVelocity = newVel;
+
+    }
 }
