af1584f110cdd682ec1ac3ae12e7e7be52d2c506
joobeck <joobeck@Hotmail.com>
2014-11-06 15:00:07 -0700

    Added in the layout of the script for the movement.  Attached it to the main miner

---
+++ Assets/PlayerMovmentScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovmentScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()
+	{
+		Vector3 newVel = this.rigidbody.velocity;
+		int xvel = 0, yvel = 0, zvel = 0;
+		if(this.rigidbody.velocity.x > 0)
+			xvel = 1;
+		if(this.rigidbody.velocity.x < 0)
+			xvel = -1;
+		
+		if(this.rigidbody.velocity.y > 0)
+			yvel = 1;
+		if(this.rigidbody.velocity.y < 0)
+			yvel = -1;
+		
+		if(this.rigidbody.velocity.z > 0)
+			zvel = 1;
+		if(this.rigidbody.velocity.z < 0)
+			zvel = -1;
+
+	}
+}
