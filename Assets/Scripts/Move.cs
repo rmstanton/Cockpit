60adb067a8b11ce7d583b5aac273a1659c83c9f8
rmstanton <rmstanton01@gmail.com>
2014-11-06 00:40:45 -0700

    spinning pretty roid things

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	public Vector3 speed = new Vector3(20, 20, 20);
+	private Vector3 movement;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement = new Vector3(Input.GetAxis("Horizontal") * speed.x,
+		                       Input.GetAxis("Jump") * speed.z,
+		                       Input.GetAxis ("Vertical") * speed.y);
+	}
+
+
+
+	void FixedUpdate(){
+		rigidbody.velocity = movement;
+	}
+}
---
+++ Assets/Scripts/SpawnAsteroids.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnAsteroids : MonoBehaviour {
+	//Vector3 speed = new Vector3(20,20,20);
+	//public Transform asteroid;
+	public int aCount = 20;
+	public float range = 20;
+	public GameObject roids;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < aCount; i++) {
+			//roids = Instantiate(Resources.Load ("Asteroid")) as GameObject;
+			roids = (GameObject) Instantiate(roids);
+			roids.transform.parent = this.transform;
+			roids.transform.position = new Vector3(
+				Random.Range (-range, range),
+				Random.Range (-range, range),
+				Random.Range (-range, range) );
+
+			roids.transform.Rotate(new Vector3(
+				Random.Range (-180, 180),
+				Random.Range (-180, 180),
+				Random.Range (-180, 180)
+				));
+
+			roids.transform.GetComponent<AsteroidMove>().rotate = new Vector3(Random.Range (-5, 5),
+			                                                                  Random.Range (-5, 5),
+			                                                                  Random.Range (-5, 5));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+			
+			
+	}
+}
+
