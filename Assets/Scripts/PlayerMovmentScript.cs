ba92f323c0bbc6a6697b2815c23fb2c14b841205
joobeck <joobeck@hotmail.com>
2014-11-07 03:23:59 -0700

    Redo Movement
    Movement(minus y Movement) redone after source tree glitched, not near
    as thoroughly tested, but think functionality works like before.

---
+++ Assets/Scripts/PlayerMovmentScript.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovmentScript : MonoBehaviour
+{
+
+    public bool dontPassZero = true;
+	
+	private bool canChangeX = true;
+	private bool canChangeY = true;
+	private bool canChangeZ = true;
+	private bool canChangeYaw = true;
+	private bool canChangePitch = true;
+	private bool canChangeRoll = true;
+	
+	private Vector3 angularVelocity = new Vector3(0,0,0);
+	
+	public float angularDampener = 60.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+    
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    void FixedUpdate()
+    {
+        AdjustVelocity();
+        AdjustRotation();
+    }
+
+    private void AdjustVelocity()
+    {
+        
+        Vector3 newVel = this.rigidbody.velocity;
+
+        //Debug.Log("Thrust is " + Input.GetAxis("Thrust"));
+        //Debug.Log("YMovement is " + Input.GetAxis("YMovement"));
+		
+		newVel.x += Input.GetAxis("Thrust") * Mathf.Cos(transform.rotation.eulerAngles.y * Mathf.Deg2Rad) * Mathf.Cos(transform.rotation.eulerAngles.z * Mathf.Deg2Rad);
+		newVel.z -= Input.GetAxis("Thrust") * Mathf.Sin(transform.rotation.eulerAngles.y * Mathf.Deg2Rad) * Mathf.Cos(transform.rotation.eulerAngles.z * Mathf.Deg2Rad);
+		newVel.y += Input.GetAxis("Thrust") * Mathf.Sin(transform.rotation.eulerAngles.z * Mathf.Deg2Rad);
+		//newVel.x += Input.GetAxis("YMovement");
+		//newVel.y += Input.GetAxis("YMovement");
+		//newVel.z += Input.GetAxis("YMovement");
+		
+		if(newVel.x - rigidbody.velocity.x == 0)
+			canChangeX = true;
+		if(newVel.y - rigidbody.velocity.y == 0)
+			canChangeY = true;
+		if(newVel.z - rigidbody.velocity.z == 0)
+			canChangeZ = true;
+        
+        if(dontPassZero)
+        {
+			if((this.rigidbody.velocity.x < 0 && newVel.x > 0) || (this.rigidbody.velocity.x > 0 && newVel.x < 0))
+				canChangeX = false;
+			if((this.rigidbody.velocity.y < 0 && newVel.y > 0) || (this.rigidbody.velocity.y > 0 && newVel.y < 0))
+				canChangeY = false;
+			if((this.rigidbody.velocity.z < 0 && newVel.z > 0) || (this.rigidbody.velocity.z > 0 && newVel.z < 0))
+				canChangeZ = false;
+        }
+        
+		if(!canChangeX)
+			newVel.x = 0;
+		if(!canChangeY)
+			newVel.y = 0;
+		if(!canChangeZ)
+			newVel.z = 0;
+
+        
+        this.rigidbody.velocity = newVel;
+    }
+
+    private void AdjustRotation()
+    {
+        
+        Vector3 newVel = angularVelocity;
+        
+        //Debug.Log("Roll is " + Input.GetAxis("Roll"));
+        //Debug.Log("Yaw is " + Input.GetAxis("Yaw"));
+        //Debug.Log("Pitch is " + Input.GetAxis("Pitch"));
+        //Debug.Log("");
+        
+        newVel.x += Input.GetAxis("Roll");
+        newVel.y += Input.GetAxis("Yaw");
+		newVel.z += Input.GetAxis("Pitch");
+		
+		if(newVel.x - angularVelocity.x == 0)
+			canChangeRoll = true;
+		if(newVel.y - angularVelocity.y == 0)
+			canChangeYaw = true;
+		if(newVel.z - angularVelocity.z == 0)
+			canChangePitch = true;
+        
+        if(dontPassZero)
+        {
+			if((angularVelocity.x < 0 && newVel.x > 0) || (angularVelocity.x > 0 && newVel.x < 0))
+				canChangeRoll = false;
+			if((angularVelocity.y < 0 && newVel.y > 0) || (angularVelocity.y > 0 && newVel.y < 0))
+				canChangeYaw = false;
+			if((angularVelocity.z < 0 && newVel.z > 0) || (angularVelocity.z > 0 && newVel.z < 0))
+				canChangePitch = false;
+        }
+		if(!canChangeRoll)
+			newVel.x = 0;
+		if(!canChangeYaw)
+			newVel.y = 0;
+		if(!canChangePitch)
+			newVel.z = 0;
+        
+        
+        angularVelocity = newVel;
+        
+        transform.Rotate(newVel/angularDampener);
+
+    }
+}
