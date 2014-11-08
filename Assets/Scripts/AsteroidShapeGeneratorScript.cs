fb46d7d0f1d8a4d19b4b3e0c58db7d4928a1cd0c
joobeck <joobeck@Hotmail.com>
2014-11-07 17:32:14 -0700

    Generation Modification
    Added in delay for when astroid size is extremely large, as well
    greatly improved performance

--- Assets/Scripts/AsteroidShapeGeneratorScript.cs
+++ Assets/Scripts/AsteroidShapeGeneratorScript.cs
@@ -4,13 +4,16 @@ using System.Collections;
 public class AsteroidShapeGeneratorScript : MonoBehaviour
 {
     public float generateReduction = 0.75f;
+    private bool[,,] positioning = new bool[101, 101, 101];
 
     // Use this for initialization
     void Start()
     {
-        if(generateReduction > 0.75)
-            generateReduction = 0.75f;
-        Generate(0, 0, 0, 1.0f);
+        if(generateReduction > 0.95)
+            generateReduction = 0.95f;
+        else if(generateReduction < 0)
+                generateReduction = 0.0f;
+        StartCoroutine(Generate(0, 0, 0, generateReduction));
     }
     
     // Update is called once per frame
@@ -19,35 +22,46 @@ public class AsteroidShapeGeneratorScript : MonoBehaviour
     
     }
 
-    void Generate(int x, int y, int z, float generateChance)
+    IEnumerator Generate(int x, int y, int z, float generateChance)
     {
-        Debug.Log("Generate Chance: " + generateChance);
-        Debug.Log("Generate Reduct: " + generateReduction);
-        Debug.Log("Combined Chance: " + generateChance * generateReduction);
-        if(HasChild(x, y, z))
-            return;
-        GameObject chunk = GameObject.Find("AsteroidChunk");
-        GameObject child = Instantiate(chunk) as GameObject;
-        child.transform.parent = this.transform;
-        child.transform.position = new Vector3(x, y, z);
+        if(generateReduction>0.9f)
+            yield return new WaitForSeconds( 10 * (generateReduction-0.9f) * generateReduction * Random.Range(0.0f, 1.0f) * (1-generateChance) * (1-generateChance));
+        if(x > 50 || x < -50 || y > 50 || y < -50 || z > 50 || z < -50)
+        {
+        }
+        else if(HasChild(x, y, z))
+        {
+        }
+        else
+        {
+            GameObject chunk = GameObject.Find("AsteroidChunk");
+            GameObject child = Instantiate(chunk) as GameObject;
+            child.transform.parent = this.transform;
+            child.transform.position = new Vector3(x, y, z);
+
+            SetChild(x, y, z);
         
-        if(Random.Range(0.0f, 1.0f) < generateChance)
-            Generate(x+1, y, z, generateChance * generateReduction);
-        if(Random.Range(0.0f, 1.0f) < generateChance)
-            Generate(x-1, y, z, generateChance * generateReduction);
-        if(Random.Range(0.0f, 1.0f) < generateChance)
-            Generate(x, y+1, z, generateChance * generateReduction);
-        if(Random.Range(0.0f, 1.0f) < generateChance)
-            Generate(x, y-1, z, generateChance * generateReduction);
-        if(Random.Range(0.0f, 1.0f) < generateChance)
-            Generate(x, y, z+1, generateChance * generateReduction);
-        if(Random.Range(0.0f, 1.0f) < generateChance)
-            Generate(x, y, z-1, generateChance * generateReduction);
+            if(Random.Range(0.0f, 1.0f) < generateChance)
+                StartCoroutine(Generate(x + 1, y, z, generateChance * generateReduction));
+            if(Random.Range(0.0f, 1.0f) < generateChance)
+                StartCoroutine(Generate(x - 1, y, z, generateChance * generateReduction));
+            if(Random.Range(0.0f, 1.0f) < generateChance)
+                StartCoroutine(Generate(x, y + 1, z, generateChance * generateReduction));
+            if(Random.Range(0.0f, 1.0f) < generateChance)
+                StartCoroutine(Generate(x, y - 1, z, generateChance * generateReduction));
+            if(Random.Range(0.0f, 1.0f) < generateChance)
+                StartCoroutine(Generate(x, y, z + 1, generateChance * generateReduction));
+            if(Random.Range(0.0f, 1.0f) < generateChance)
+                StartCoroutine(Generate(x, y, z - 1, generateChance * generateReduction));
+        }
     }
 
     bool HasChild(int x, int y, int z)
     {
-        Debug.Log("In hasChild(" + x + "," + y + "," + z + ")");
+        if(x > 50 || x < -50 || y > 50 || y < -50 || z > 50 || z < -50)
+            return false;
+        return positioning[x + 50, y + 50, z + 50];
+        /*
         foreach(Transform child in transform)
         {
             if(child.transform.position.x == x
@@ -55,7 +69,11 @@ public class AsteroidShapeGeneratorScript : MonoBehaviour
             && child.transform.position.z == z)
                 return false;
         }
-        Debug.Log("returning false");
-        return false;
+        return false;*/
+    }
+
+    void SetChild(int x, int y, int z, bool set = true)
+    {
+        positioning[x + 50, y + 50, z + 50] = set;
     }
 }
