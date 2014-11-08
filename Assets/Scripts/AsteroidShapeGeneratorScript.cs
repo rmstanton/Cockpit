1b60042eb45966e717a2a13a82bfee6453866bd7
joobeck <joobeck@Hotmail.com>
2014-11-07 19:27:53 -0700

    Adjusted more spawning parameters

--- Assets/Scripts/AsteroidShapeGeneratorScript.cs
+++ Assets/Scripts/AsteroidShapeGeneratorScript.cs
@@ -4,29 +4,33 @@ using System.Collections;
 public class AsteroidShapeGeneratorScript : MonoBehaviour
 {
     public float generateReduction = 0.75f;
-    private bool[,,] positioning = new bool[101, 101, 101];
+    private static int width = 30;
+    private bool[,,] positioning = new bool[2*width+1, 2*width+1, 2*width+1];
+    //int counter = 0;
 
     // Use this for initialization
     void Start()
     {
-        if(generateReduction > 0.95)
-            generateReduction = 0.95f;
+        if(generateReduction > 0.94)
+            generateReduction = 0.94f;
         else if(generateReduction < 0)
-                generateReduction = 0.0f;
+            generateReduction = 0.0f;
         StartCoroutine(Generate(0, 0, 0, generateReduction));
+        StartCoroutine(UpdateMass());
     }
     
     // Update is called once per frame
     void Update()
     {
-    
+        //if(counter++ % 100 == 0)
+            //UpdateMass();
     }
 
     IEnumerator Generate(int x, int y, int z, float generateChance)
     {
-        if(generateReduction>0.9f)
-            yield return new WaitForSeconds( 10 * (generateReduction-0.9f) * generateReduction * Random.Range(0.0f, 1.0f) * (1-generateChance) * (1-generateChance));
-        if(x > 50 || x < -50 || y > 50 || y < -50 || z > 50 || z < -50)
+        if(generateReduction - generateChance > 0.6f)
+            yield return new WaitForSeconds(Random.Range(0.0f, 2.0f) * (generateReduction - generateChance));
+        if(x > width || x < -1*width || y > width || y < -1*width || z > width || z < -1*width)
         {
         }
         else if(HasChild(x, y, z))
@@ -37,30 +41,42 @@ public class AsteroidShapeGeneratorScript : MonoBehaviour
             GameObject chunk = GameObject.Find("AsteroidChunk");
             GameObject child = Instantiate(chunk) as GameObject;
             child.transform.parent = this.transform;
-            child.transform.position = new Vector3(x, y, z);
+            child.transform.localRotation = Quaternion.identity;
+            child.transform.localPosition = new Vector3(x, y, z);
 
             SetChild(x, y, z);
         
             if(Random.Range(0.0f, 1.0f) < generateChance)
                 StartCoroutine(Generate(x + 1, y, z, generateChance * generateReduction));
+            //if(generateReduction > 0.8f)
+                //yield return new WaitForSeconds(Random.Range(0.0f, 0.05f));
             if(Random.Range(0.0f, 1.0f) < generateChance)
                 StartCoroutine(Generate(x - 1, y, z, generateChance * generateReduction));
+            //if(generateReduction > 0.8f)
+                //yield return new WaitForSeconds(Random.Range(0.0f, 0.05f));
             if(Random.Range(0.0f, 1.0f) < generateChance)
                 StartCoroutine(Generate(x, y + 1, z, generateChance * generateReduction));
+            //if(generateReduction > 0.8f)
+                //yield return new WaitForSeconds(Random.Range(0.0f, 0.05f));
             if(Random.Range(0.0f, 1.0f) < generateChance)
                 StartCoroutine(Generate(x, y - 1, z, generateChance * generateReduction));
+            //if(generateReduction > 0.8f)
+                //yield return new WaitForSeconds(Random.Range(0.0f, 0.05f));
             if(Random.Range(0.0f, 1.0f) < generateChance)
                 StartCoroutine(Generate(x, y, z + 1, generateChance * generateReduction));
+            //if(generateReduction > 0.8f)
+                //yield return new WaitForSeconds(Random.Range(0.0f, 0.05f));
             if(Random.Range(0.0f, 1.0f) < generateChance)
                 StartCoroutine(Generate(x, y, z - 1, generateChance * generateReduction));
         }
+        yield return new WaitForSeconds(0);
     }
 
     bool HasChild(int x, int y, int z)
     {
-        if(x > 50 || x < -50 || y > 50 || y < -50 || z > 50 || z < -50)
+        if(x > width || x < -1*width || y > width || y < -1*width || z > width || z < -1*width)
             return false;
-        return positioning[x + 50, y + 50, z + 50];
+        return positioning[x + width, y + width, z + width];
         /*
         foreach(Transform child in transform)
         {
@@ -74,6 +90,22 @@ public class AsteroidShapeGeneratorScript : MonoBehaviour
 
     void SetChild(int x, int y, int z, bool set = true)
     {
-        positioning[x + 50, y + 50, z + 50] = set;
+        positioning[x + width, y + width, z + width] = set;
+    }
+
+    IEnumerator UpdateMass()
+    {
+        yield return new WaitForSeconds(1.0f);
+        int mass = 0;
+        for(int i=0; i<2*width+1; i++)
+            for(int j=0; j<2*width+1; j++)
+                for(int k=0; k<2*width+1; k++)
+                    if(positioning[i, j, k])
+                        mass++;
+        if(mass > 0)
+            this.rigidbody.mass = mass;
+        else
+            this.rigidbody.mass = 1;
+        StartCoroutine(UpdateMass());
     }
 }
