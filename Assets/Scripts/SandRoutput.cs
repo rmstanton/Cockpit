58686907978ab75bcd18e7b18b1c94ce30911202
joobeck <joobeck@hotmail.com>
2014-11-10 03:03:30 -0700

    Added Window Text

--- Assets/Scripts/SandRoutput.cs
+++ Assets/Scripts/SandRoutput.cs
@@ -2,14 +2,14 @@
 using System.Collections;
 
 public class SandRoutput : MonoBehaviour {
-	public float speed;
-	public float rotation;
-	void OnGUI(){
+	public Vector3 speed;
+	public Vector3 rotation;
+	/*void OnGUI(){
 		GUI.TextArea(new Rect(10,Screen.height - 20,50,20),speed.ToString());
 		GUI.TextArea(new Rect(10,Screen.height - 40,50,20),"Speed");
 		GUI.TextArea (new Rect (10, Screen.height - 60, 50, 20), rotation.ToString());
 		GUI.TextArea (new Rect (10, Screen.height - 80, 50, 20), "AngRot.");
-	}
+	}*/
 	// Use this for initialization
 	void Start () {
 	
@@ -19,7 +19,7 @@ public class SandRoutput : MonoBehaviour {
 	void Update () {
 		
 		//output = gameObject.GetComponent<SandRoutput> ();
-		speed = this.rigidbody.velocity.magnitude;
-		rotation = this.rigidbody.angularVelocity.magnitude;
+		speed = this.rigidbody.velocity;
+		rotation = this.rigidbody.angularVelocity;
 	}
 }
