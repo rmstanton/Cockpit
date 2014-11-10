fd3ebe4954dbc0e344a13512f502bb9d64901b06
rmstanton <rmstanton01@gmail.com>
2014-11-09 22:08:32 -0700

    Random Asteroid Spawning + moving
    but oooooooh LORD they're slow

--- Assets/Scripts/AsteroidMove.cs
+++ Assets/Scripts/AsteroidMove.cs
@@ -4,15 +4,20 @@ using System.Collections;
 public class AsteroidMove : MonoBehaviour {
 	public Vector3 move;
 	public Vector3 rotate = new Vector3(0,0,0);
+	public GameObject Player;
 
 	// Use this for initialization
 	void Start () {
-	
+		Player = GameObject.Find ("Main Camera");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (rotate != null)
 			transform.Rotate (rotate);
+
+		if (Vector3.Distance (this.transform.position, Player.transform.position) > 500) {
+			Destroy(this.gameObject);		
+		}
 	}
 }
--- Assets/Scripts/SpawnAsteroids.cs
+++ Assets/Scripts/SpawnAsteroids.cs
@@ -6,12 +6,17 @@ public class SpawnAsteroids : MonoBehaviour {
 	//public Transform asteroid;
 	public int aCount = 20;
 	public float range = 20;
-	public GameObject roids;
+	public GameObject roidsOrig;
+	public const int FRAMES = 60;
+	public float initRotation;
+
 	// Use this for initialization
 	void Start () {
 		for (int i = 0; i < aCount; i++) {
 			//roids = Instantiate(Resources.Load ("Asteroid")) as GameObject;
-			roids = (GameObject) Instantiate(roids);
+			GameObject roids = Instantiate(roidsOrig) as GameObject;
+			roids.GetComponent<AsteroidShapeGeneratorScript>().generateReduction = Random.Range(0.5f,0.9f);
+
 			roids.transform.parent = this.transform;
 			roids.transform.position = new Vector3(
 				Random.Range (-range, range),
@@ -24,15 +29,27 @@ public class SpawnAsteroids : MonoBehaviour {
 				Random.Range (-90, 90)
 				));
 
-			roids.transform.GetComponent<AsteroidMove>().rotate = new Vector3(Random.Range (-5, 5),
-			                                                                  Random.Range (-5, 5),
-			                                                                  Random.Range (-5, 5));
+
+			roids.transform.GetComponent<AsteroidMove>().rotate = new Vector3(Random.Range (-initRotation, initRotation),
+			                                                                  Random.Range (-initRotation, initRotation),
+			                                                                  Random.Range (-initRotation, initRotation));
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-			
+		if (Random.Range (1, 30 * FRAMES) == 1 /*!testSpawn*/) {
+			GameObject roid = Instantiate (roidsOrig) as GameObject;
+			roid.transform.position = new Vector3(
+				Random.Range (-range, range),
+				Random.Range (-range, range),
+				Random.Range (-range, range) );
+
+			roid.rigidbody.velocity = new Vector3(100,
+			                                      0,
+			                                      0);
+		}
+
 			
 			
 	}
