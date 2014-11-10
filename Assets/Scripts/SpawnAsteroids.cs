9e2346a3a8d7e6bb8b17ac695b0746121d1610be
rmstanton <rmstanton01@gmail.com>
2014-11-09 23:48:49 -0700

    Asteroid edge spawning, adjust mover speed

--- Assets/Scripts/SpawnAsteroids.cs
+++ Assets/Scripts/SpawnAsteroids.cs
@@ -8,10 +8,13 @@ public class SpawnAsteroids : MonoBehaviour {
 	public float range = 20;
 	public GameObject roidsOrig;
 	public int spawnDistance;
+	public int avgSpawnsPerSecond = 10;
 	public const int FRAMES = 60;
+	public int moverSpawnRange = 250;
+	public int moverMaxSpeed = 100;
 	public float initRotation;
-	public bool testTargeted = false, testSent = false;
-	public Vector3 testDest;
+
+
 
 
 	// Use this for initialization
@@ -26,10 +29,7 @@ public class SpawnAsteroids : MonoBehaviour {
 				Random.Range (-range, range),
 				Random.Range (-range, range) );
 
-			if (!testTargeted){
-				testDest = roids.transform.position;
-				testTargeted = true;
-			}
+
 
 			roids.transform.Rotate(new Vector3(
 				Random.Range (-90, 90),
@@ -42,31 +42,70 @@ public class SpawnAsteroids : MonoBehaviour {
 			                                                                  Random.Range (-initRotation, initRotation),
 			                                                                  Random.Range (-initRotation, initRotation));*/
 
-			roids.rigidbody.angularVelocity = new Vector3(Random.Range (-initRotation, initRotation),
-			                                              Random.Range (-initRotation, initRotation),
-			                                              Random.Range (-initRotation, initRotation));
+			roids.rigidbody.angularVelocity = new Vector3(Random.Range (-initRotation/2, initRotation/2) + Random.Range (-initRotation/2, initRotation/2),
+			                                              Random.Range (-initRotation/2, initRotation/2) + Random.Range (-initRotation/2, initRotation/2),
+			                                              Random.Range (-initRotation/2, initRotation/2) + Random.Range (-initRotation/2, initRotation/2));
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Random.Range (1, 10 * FRAMES) == 1 /*!testSpawn*/) {
-			GameObject roid = Instantiate (roidsOrig) as GameObject;
-			roid.transform.position = new Vector3(
-				Random.Range (-range, range),
-				Random.Range (-range, range),
-				Random.Range (-range, range) );
 
-			roid.rigidbody.velocity = new Vector3(Random.Range (-100, 100),
-			                                      Random.Range (-100, 100),
-			                                      Random.Range (-100, 100));
+
+		if (Random.Range (1, avgSpawnsPerSecond * FRAMES) == 1 /*!testSpawn*/) {
+			int spawnCase = Random.Range (1, 6);
+			GameObject roid = Instantiate (roidsOrig) as GameObject;
+			roid.GetComponent<AsteroidShapeGeneratorScript>().generateReduction = Random.Range(0.05f,0.5f);
+
+			switch (spawnCase){
+			case 1:
+				roid.transform.position = new Vector3 (moverSpawnRange, 
+				                                       Random.Range (-moverSpawnRange, moverSpawnRange),
+				                                       Random.Range (-moverSpawnRange, moverSpawnRange));
+				break;
+			
+			case 2:
+				roid.transform.position = new Vector3 (-moverSpawnRange, 
+				                                       Random.Range (-moverSpawnRange, moverSpawnRange),
+				                                       Random.Range (-moverSpawnRange, moverSpawnRange));
+				break;
+			case 3:
+				roid.transform.position = new Vector3 (Random.Range (-moverSpawnRange, moverSpawnRange), 
+				                                       moverSpawnRange,
+				                                       Random.Range (-moverSpawnRange, moverSpawnRange));
+				break;
+			case 4:
+				roid.transform.position = new Vector3 (Random.Range (-moverSpawnRange, moverSpawnRange), 
+				                                       -moverSpawnRange,
+				                                       Random.Range (-moverSpawnRange, moverSpawnRange));
+				break;
+			case 5:
+				roid.transform.position = new Vector3 (Random.Range (-moverSpawnRange, moverSpawnRange), 
+				                                       Random.Range (-moverSpawnRange, moverSpawnRange),
+				                                       moverSpawnRange);
+				break;
+
+			case 6:
+				roid.transform.position = new Vector3 (Random.Range (-moverSpawnRange, moverSpawnRange), 
+				                                       Random.Range (-moverSpawnRange, moverSpawnRange),
+				                                       -moverSpawnRange);
+				break;
+			default:
+				Debug.Log ("Spawn Update default case ");
+				roid.transform.position = new Vector3 (Random.Range (-moverSpawnRange, moverSpawnRange), 
+				                                       Random.Range (-moverSpawnRange, moverSpawnRange),
+				                                       Random.Range (-moverSpawnRange, moverSpawnRange));
+				break;
+			}
+
+
+
+			roid.rigidbody.velocity = new Vector3(Random.Range (-moverMaxSpeed, moverMaxSpeed),
+			                                      Random.Range (-moverMaxSpeed, moverMaxSpeed),
+			                                      Random.Range (-moverMaxSpeed, moverMaxSpeed));
 		}
 
-		/*if (!testSent) {
-			GameObject testRoid = Instantiate(roidsOrig) as GameObject;
-			testRoid.transform.position = new Vector3 (20, 20, 20);
 
-		}*/
 
 			
 	}
